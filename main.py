e8d0a797dac11b12cd4de44b419d2883f71c72cf
kasp214a <kasperheideder@gmail.com>
2024-08-04 13:51:57 +0200

    opdatering til landings siden for systemet

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ app.include_router(lokations_modul.router)
 
 @app.get("/")
 async def root():
-    return {"message" : "Velkommen til klik og hent systemer."}
+    return {
+        "Velkomst message" : "Velkommen til klik og hent systemer.",
+        "info" : "here er en liste af de paths der kan til gås",
+        "/docs" : "docomentation siden hvor det er muligt at genere ordre og ændre status på eksiternde ordre",
+        "/ordre" : "viser alle ordre i systemer og deres ordre linjer",
+        "/ordre/aktiv" : "viser all aktive ordre i systemet",
+        "/ordre/'ordrenummer'" : "viser information om ordren med det givede ordrenummer",
+        "/ordre/'ordrenummer'/'ordrelinje_ID'" : "viser ordrelinjen fra den valge ordre"
+                  
+        }
 
